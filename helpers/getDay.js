e12ec62d79c382d4c703583587c01a3f5799788d
Nick Jennings <nick@silverbucket.net>
2021-05-21 00:29:23 +0200

    date helpers

--- helpers/getDay.js
+++ helpers/getDay.js
@@ -4,15 +4,3 @@ module.exports = function (date) {
     return date.getDate();
   }
 };
-
-// function parseDate(files, metalsmith, done) {
-//   for (let i = 0, len = files.length; i < len; i += 1) {
-//     if (files[i].date) {
-//       const date = new Date(files[i].date);
-//       files[i].year = date.getFullYear();
-//       files[i].month = date.getMonth()+1;
-//       files[i].day = date.getDate();
-//     }
-//   }
-//   done();
-// }
