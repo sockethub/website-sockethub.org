d77bbaa1705e3a51a0551a21f69ac866b84a508d
Nick Jennings <nick@silverbucket.net>
2021-05-21 01:03:02 +0200

    deploy to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #git subtree push --prefix build/ origin gh-pages
-git add *
-git commit -m "udpate website" .
-git push
+#git add *
+#git commit -m "udpate website" .
+#git push
 git push origin `git subtree split --prefix build/ master`:gh-pages --force
